e48b2cb51e563153950789072b9dd192626a002c
Gaurip10 <thegauriparhad@gmail.com>
2021-09-01 19:02:08 +0530

    Added: Adding valid last username

--- src/main/java/com/brideglabz/UserRegistartion.java
+++ src/main/java/com/brideglabz/UserRegistartion.java
@@ -2,24 +2,24 @@ package com.brideglabz;
 publimport java.util.regex.*;
 
 class UserRegistartion {
-    public static boolean isValidFirstUsername(String name)
-{
-    String regex="^[Cap]\\w{5,29}[A-Za-z]$";
+    public static boolean isValidlastUsername(String name)
+    {
+        String regex="^[Cap]\\w{5,29}[A-Za-z]$";
 
-    Pattern p=Pattern.compile(regex);
+        Pattern p=Pattern.compile(regex);
 
-    if (name ==null){
-        return false;
+        if (name ==null){
+            return false;
+        }
+
+        //Pattern class contains matcher() method
+        //to find matching between given username
+        //and regular expression
+        Matcher m =p.matcher(name);
+
+        //Return if the username
+        return m.matches();
     }
-
-    //Pattern class contains matcher() method
-    //to find matching between given username
-    //and regular expression
-    Matcher m =p.matcher(name);
-
-    //Return if the username
-    return m.matches();
-}
     public static void main(String[] args)
     {
         String str1="INDIA";
@@ -31,5 +31,21 @@ class UserRegistartion {
         String str3="Cap2314";
         System.out.println(isValidFirstUsername(str3));
     }
-}
+    public static void main(String[] args) {
+        //Checking for lastusername
 
+        //Test Case:1
+        String str1 = "Maharashtra";
+        System.out.println(isValidlastUsername(str1));
+
+        //Test case 2
+        String str2 = "Cap434abc";
+        System.out.println(isValidlastUsername(str2));
+
+        //Test Case:3
+        String str3 = "Cap231xyz";
+        System.out.println(isValidlastUsername(str3));
+    }
+
+}
+    
